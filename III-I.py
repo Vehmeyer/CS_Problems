7007008968bbd0856ea430fb6bb9b60ab5fc7a9f
Vehmeyer <j.vehmeyer@gmail.com>
2021-09-14 13:32:21 -0500

    module III-II

--- III-I.py
+++ III-I.py
@@ -33,3 +33,36 @@ def insertValueIntoSortedLinkedList(l, value):
 #Given two singly linked lists sorted in non-decreasing order, your task is to merge them. In other words, return a singly linked list, 
 #also sorted in non-decreasing order, that contains the elements from both original lists.
 
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def mergeTwoLinkedLists(l1, l2):
+    dummy_node = ListNode(0)
+    last_result_node = dummy_node
+    
+    while True:
+        if l1 is None:
+            last_result_node.next = l2
+            break
+        elif l2 is None:
+            last_result_node.next = l1
+            break
+            
+        if l1.value <= l2.value:
+            if l1:
+                new_node = l1
+                l1 = l1.next
+                new_node.next = last_result_node.next
+                last_result_node.next = new_node
+        elif l2:
+            new_node = l2
+            l2 = l2.next
+            new_node.next = last_result_node.next
+            last_result_node.next = new_node
+        
+        last_result_node = last_result_node.next
+        
+    return dummy_node.next
